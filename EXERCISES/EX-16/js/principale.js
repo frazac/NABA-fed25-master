2160554c136e2cbbe6a2091d60f08fe2a68cef85
frazac <frazac@users.noreply.github.com>
2025-06-10 19:22:06 +0200

    javascript

---
+++ EXERCISES/EX-16/js/principale.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Il codice qui dentro verrà eseguito quando la pagina è completamente caricata
+    // The code inside here will run when the page is fully loaded
+
+    var block = document.getElementById('pippo');
+
+    block.addEventListener('click', function() {
+    console.log(block);
+    if (block.getAttribute('class') == 'red') block.setAttribute('class', 'blue');
+    else block.setAttribute('class', 'red');
+    });
+
+});
+
